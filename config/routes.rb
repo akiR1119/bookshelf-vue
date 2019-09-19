de53f5e78a4732ec4048a375b407503b235b889f
Akito <kuroya1993@gmail.com>
2019-09-20 01:10:10 +0900

    トップページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'shelves#index'
+  root to: 'home#index'
   resources :users do
     resources :items, only: [:index, :new, :create, :show, :edit]
     resources :shelves, only: [:index, :new, :create, :show, :edit]
